de0bec5f30f467c796c1d61658448f766b771fbb
CoderGirl <angi.adema@gmail.com>
2023-01-08 17:21:32 -0700

    updated Navbar.js, added all component files, updated index.html and App.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-function BasicExample() {
+export default function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+        <Navbar.Brand href="#home"></Navbar.Brand>Angela E. Adema
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
             <Nav.Link href="aboutme">About Me</Nav.Link>
             <Nav.Link href="portfolio">Portfolio</Nav.Link>
             <Nav.Link href="contact">Contact</Nav.Link>
@@ -21,4 +21,4 @@ function BasicExample() {
   );
 }
 
-export default BasicExample;
+
