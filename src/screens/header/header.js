b234ac31649d629a53510eeaac834eec17df269e
CoderGirl <angi.adema@gmail.com>
2023-01-11 14:56:52 -0700

    added header and updated font

--- src/screens/header/header.js
+++ src/screens/header/header.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import Typed from 'react-typed';
+import React from "react";
+import Typed from "react-typed";
+import "./header.css";
 
 function header() {
   return (
-    <div>
+    <div className="main-info">
       <h1>What Am I?</h1>
-      <Typed 
+      <Typed
         strings={[
-            '',
-            'Software Engineer',
-            'Also referred to as...',
-            'A Front-End Developer',
-            'A Back-End Developer',
-            'A MERN Full Stack Developer',
-            'OR!!!',
-            'A Coder!',
-            '',
-            '']}
-            typeSpeed={40}
-            backSpeed={50}
-            loop
+          "",
+          "Software Engineer",
+          "Also referred to as...",
+          "A Front-End Developer",
+          "A Back-End Developer",
+          "A MERN Full Stack Developer",
+          "OR!!!",
+          "A Coder!",
+          "",
+          "",
+        ]}
+        typeSpeed={40}
+        backSpeed={50}
+        loop
       />
     </div>
-  )
+  );
 }
 
-export default header
+export default header;
