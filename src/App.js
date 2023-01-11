13068b934540f2c3e77ff6a2e73dacbb2f8d30d7
CoderGirl <angi.adema@gmail.com>
2023-01-11 13:27:42 -0700

    completed the particle animation and added Particles.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navigation from "./screens/navigationBar/NavigationBar";
+import Particles from './Particles';
 
 export default function App() {
   return (
     <div className="App">
       <Navigation />
+      <Particles />
     </div>
   );
 }
