f57463b1b9171830e4e1624eeb47e23b53e4cb18
CoderGirl <angi.adema@gmail.com>
2023-01-12 12:51:03 -0700

    added project icons and created portfolioData.js

---
+++ src/helpers/portfolioData.js
@@ -0,0 +1,54 @@
+import truckDB from "../images/projectsicon/truckDbSS.png";
+import mvcBlog from "../images/projectsicon/mvcTechBlogSS.png";
+import socialNetwork from "../images/projectsicon/socialNetworkSS.png";
+import teamProfile from "../images/projectsicon/team-profile-managerSS.png";
+import textEditor from "../images/projectsicon/textEditorSS.png";
+import weatherApp from "../images/projectsicon/weatherappSS.png";
+
+const portfolioData = [
+  {
+    images: "truckDB",
+    link: "https://calm-stream-98427.herokuapp.com/",
+    desc: "Truck Database",
+    summary: "An app to look up and locate trucks and parts.",
+    tech: "MySQL/MySQL2 | Node.js/Express.js | Sequelize | JavaScript",
+  },
+  {
+    images: "mvcBlog",
+    link: "https://guarded-peak-34864.herokuapp.com/",
+    desc: "Tech Blog",
+    summary: "A blog with posts regarding new technology.",
+    tech: "MySQL/MySQL2 | Node.js/Express.js | Sequelize | Handlebars.js | JavaScript",
+  },
+  {
+    images: "socialNetwork",
+    link: "https://github.com/Angi-Adema/Social-Network-API",
+    desc: "Back-end Social Network API",
+    summary: "This app is the back-end piece for a social network API.",
+    tech: "NoSQL | Node.js/Express.js | MongoDB/Mongoose | JavaScript",
+  },
+  {
+    images: "teamProfile",
+    link: "https://github.com/Angi-Adema/Team-Profile-Manager",
+    desc: "Team Profile",
+    summary:
+      "Built for testing, an app for managers to store their team profiles.",
+    tech: "Node.js/Express.js | Jest | JavaScript",
+  },
+  {
+    images: "textEditor",
+    link: "",
+    desc: "",
+    summary: "",
+    tech: "MySQL/MySQL2 | Node.js/Express.js | Sequelize | JavaScript",
+  },
+  {
+    images: "weatherApp",
+    link: "",
+    desc: "",
+    summary: "",
+    tech: "MySQL/MySQL2 | Node.js/Express.js | Sequelize | JavaScript",
+  },
+];
+
+export default portfolioData;
