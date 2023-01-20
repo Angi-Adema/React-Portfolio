d6c1d9bc3187d30898f49d92898a194bf392cdbf
CoderGirl <angi.adema@gmail.com>
2023-01-20 11:01:21 -0700

    updated Contact.js

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './contact.css';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import React from "react";
+import "./contact.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 function Contact() {
   return (
@@ -10,22 +10,20 @@ function Contact() {
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Name</Form.Label>
         <Form.Control type="name" placeholder="Name" />
-        <Form.Text className="text-muted">
-        </Form.Text>
+        <Form.Text className="text-muted"></Form.Text>
       </Form.Group>
-      
+
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-        </Form.Text>
+        <Form.Text className="text-muted"></Form.Text>
       </Form.Group>
 
       <InputGroup>
         <InputGroup.Text>Message</InputGroup.Text>
         <Form.Control as="textarea" aria-label="With textarea" />
       </InputGroup>
-     
+
       <Button className="button" variant="primary" type="submit">
         Submit
       </Button>
@@ -34,4 +32,3 @@ function Contact() {
 }
 
 export default Contact;
-
