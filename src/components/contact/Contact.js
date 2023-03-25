d173aae96acb4aeabaf78f0f6b34d7d6e5b7a7f7
CoderGirl <angi.adema@gmail.com>
2023-03-25 14:53:56 -0600

    corrected contact page

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,39 +6,37 @@ import InputGroup from "react-bootstrap/InputGroup";
 
 function Contact() {
   return (
-    <Form className="form_container">
+    <div className="contact_container">
       <h1>Contact Information</h1>
-      <p>
-        Please feel free to reach out directly via any of the contact
-        information below!
-      </p>
-      <br></br>
-      <p>Email: angi.adema@gmail.com</p>
-      <p>Phone: (720) 641-5568</p>
-      {/* <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="name" placeholder="Name" />
-        <Form.Text className="text-muted"></Form.Text>
-      </Form.Group>
+        <p>Please feel free to reach out directly via any of the contact information below!</p>
+        <p>Email: angi.adema@gmail.com</p>
+        <p>Phone: (720) 641-5568</p>
+    </div>
+    // <Form >
+    //    <Form.Group className="mb-3" controlId="formBasicEmail">
+    //     <Form.Label>Name</Form.Label>
+    //     <Form.Control type="name" placeholder="Name" />
+    //     <Form.Text className="text-muted"></Form.Text>
+    //   </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted"></Form.Text>
-      </Form.Group>
+    //   <Form.Group className="mb-3" controlId="formBasicEmail">
+    //     <Form.Label>Email address</Form.Label>
+    //     <Form.Control type="email" placeholder="Enter email" />
+    //     <Form.Text className="text-muted"></Form.Text>
+    //   </Form.Group>
 
-      <InputGroup>
-        <InputGroup.Text>Message</InputGroup.Text>
-        <Form.Control as="textarea" aria-label="With textarea" />
-      </InputGroup>
+    //   <InputGroup>
+    //     <InputGroup.Text>Message</InputGroup.Text>
+    //     <Form.Control as="textarea" aria-label="With textarea" />
+    //   </InputGroup>
 
-      <Button className="button" variant="primary" type="submit">
-        Submit
-      </Button> */}
-      {/* <div className="button">
+    //   <Button className="button" variant="primary" type="submit">
+    //     Submit
+    //   </Button> 
+    //   <div className="button">
 
-      </div> */}
-    </Form>
+    //   </div>
+    // </Form> 
   );
 }
 
