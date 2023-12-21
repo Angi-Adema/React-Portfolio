0fccc08829d18859ed028bd8fe56b922a3ad3648
CoderGirl <angi.adema@gmail.com>
2023-12-21 16:47:48 -0700

    again updated the resume link

--- src/components/navigationBar/NavigationBar.js
+++ src/components/navigationBar/NavigationBar.js
@@ -42,7 +42,7 @@ function NavigationBar({ currentPage, setCurrentPage }) {
           <Nav className="links" style={{ margin: "0 1rem" }}>
             {createLinks(setCurrentPage, currentPage)}
             <a
-              href="https://drive.google.com/file/d/1mcp8r5m9uYdFwgZM7d2Zbh20PmRktiVd/view?usp=sharing"
+              href="https://docs.google.com/document/d/1wC2D82TzQXVFEoZtlOY1cqjHyoUz2SKjCfX-yuIUTb0/edit?usp=sharing"
               className="nav-link"
               target="_blank"
               download
