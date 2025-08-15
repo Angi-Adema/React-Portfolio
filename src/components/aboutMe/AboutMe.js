3dceff2a7090355f23906b84a303b73aed4ffd06
Angi-Adema <dr.adema@gmail.com>
2025-08-15 11:46:12 -0600

    updated the aboutMe section with updated picture and about me section

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./aboutMe.css"
-import person from "../../images/AngiAdemaJPG.jpg"
+import person from "../../images/006-tSTreFkBwbE.jpeg"
 
 function AboutMe() {
   return (
@@ -11,37 +11,34 @@ function AboutMe() {
       <div className="about_text" style={{ marginTop: "3rem" }}>
         <h1>ABOUT ME</h1>
         <p>
-          Armed with a Bachelor of Science from the University of Colorado at
-          Denver, I embarked on a dynamic career journey that has woven together
-          diverse expertise in Human Resources, Mortgage Processing, and Web
-          Development.
+          Full-Stack Java Developer with hands-on experience building secure,
+          scalable, applications using Core Java, Spring Boot, Spring Framework,
+          JPA, JDBC, Hibernate, RESTful APIs, MySQL, and NoSQL databases. Skilled
+          in project development, debugging, problem-solving, and Agile collaboration
+          to deliver high-quality software solutions.
         </p>
         <p>
-          Driven by a passion for learning and technology, I pivoted to immerse
-          myself in the realm of web development, undertaking comprehensive
-          studies at the University of Denver. My dedication culminated in
-          achieving certification as a MERN Full-Stack Developer, equipping me
-          with a robust skill set poised to innovate and create impactful
-          digital solutions. Presently, I am working toward an Oracle Java certification.
+          My career began with a Bachelor of Science from the University of Colorado
+          at Denver, followed by roles in mortgage processing and human resources 
+          before making the pivot into software development. Completing the University
+          of Denver Full-Stack Coding Bootcamp and earning a certification in the 
+          MERN tech stack laid the groundwork for my current pursuit of the Oracle
+          Certified Professional Java SE 11 credential.
         </p>
         <p>
-          My proficiency spans an array of technologies including HTML, CSS, Java, 
-          JavaScript, Node.js, Express.js, MySQL/MySQL2, NoSQL, MongoDB, and
-          ReactJS, among others. Throughout my journey, I've employed Object
-          Oriented Programming and embraced RESTful API construction through
-          various frameworks to craft diverse projects, fostering a keen knack
-          for problem-solving and continual learning.
+          Recent work includes developing a Social Media Blog RESTful API at 
+          Revature—first with Java, Maven, JDBC, Javalin and MySQL. Then re-built
+          this project using Spring Boot, Spring Security, JPA, Hibernate and MySQL.
+          Another project recently completed for the University of Colorado at Denver
+          was refactoring a Python-based architectural modeling application, reducing 
+          the codebase by 30%, improving load time by 95% and adding enhanced features 
+          for a better user experience. 
         </p>
         <p>
-          I am fervently driven by a dual pursuit: to perpetually expand my
-          knowledge horizon and to leverage my expertise to enhance a company's
-          online footprint, amplifying its success in the digital sphere. I
-          invite you to explore my portfolio showcasing a curated collection of
-          projects, each a testament to my commitment to excellence.
-        </p>
-        <p>
-          I eagerly anticipate the opportunity to connect and explore avenues
-          where my skills and passion align with your organizational goals.
+          Passionate about continuous learning and leveraging technology to solve
+          complex problems, I aim to create impactful digital solutions that drive
+          organizational success. My portfolio showcases projects that reflect technical
+          expertise, dedication, and a commitment to excellence.
         </p>
       </div>
     </div>
